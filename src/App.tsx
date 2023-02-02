1c76f5b76186a59b25a087cbbf8d0b8a0b2ab50b
butterbeetle <butterbeetle@naver.com>
2023-02-02 22:06:14 +0900

    [004] TodoAddHandler props로 전달해서 ref값 얻어오기

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const TodoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={TodoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,16 @@
 import React, { useRef } from "react";
 
-const NewTodo: React.FC = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enterdText = textInputRef.current!.value;
-    console.log(enterdText);
+    props.onAddTodo(enterdText);
   };
 
   return (
